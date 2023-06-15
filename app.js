cf270a67ea2a700fef9b190990005577149ef5fe
Sergio Gomez C <segoco@gmail.com>
2023-06-15 17:48:23 -0500

    deploy port changed to PORT 500

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = 3000 || process.env.PORT;
+const port = 5000 || process.env.PORT;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
