2954c84ae3bfac4a08457f173d6ed2ad686563f8
Sergio Gomez C <segoco@gmail.com>
2023-06-15 19:16:02 -0500

    austes para crear un login de prueba

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express');
 
 const app = express();
-const port = 3000 || process.env.PORT;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.static(`${__dirname}/public`));
+const port = 3000 || process.env.PORT;
 
 app.listen(port, () => {
   console.info(`Fwitter backend listening at http://localhost:${port}`);
